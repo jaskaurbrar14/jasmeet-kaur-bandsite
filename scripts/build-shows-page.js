efb4cf5966635212a9317aa920faf85bc8772734
jaskaurbrar14 <jaskaurbrar14@gmail.com>
2024-01-09 03:35:19 +0530

    update form functionality fix the value for comment section change from string to the object key value

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,32 +1,93 @@
-[
+const Shows = [
   {
-    date: "Mon Sept 06 2021",
+    date: 1630866600000,
     venue: "Ronald Lane",
     location: "San Francisco, CA",
   },
   {
-    date: "Tue Sept 21 2021",
+    date: 1632162600000,
     venue: "Pier 3 East",
     location: "San Francisco, CA",
   },
   {
-    date: "Fri Oct 15 2021",
+    date: 1634236200000,
     venue: "View Lounge",
     location: "San Francisco, CA",
   },
   {
-    date: "Sat Nov 06 2021",
+    date: 1636137000000,
     venue: "Hyatt Agency",
     location: "San Francisco, CA",
   },
   {
-    date: "Fri Nov 26 2021",
+    date: 1637865000000,
     venue: "Moscow Center",
     location: "San Francisco, CA",
   },
   {
-    date: "Wed Dec 15 2021",
+    date: 1639506600000,
     venue: "Press Club",
     location: "San Francisco, CA",
   },
 ];
+function CreateShow(i) {
+  const showCont = document.querySelector(".show");
+  const showEl = document.createElement("article");
+  showEl.classList.add("show-details");
+  showCont.appendChild(showEl);
+
+  const showDate = document.createElement("section");
+  showDate.classList.add("show-details--date");
+  showEl.appendChild(showDate);
+
+  const showDateLabel = document.createElement("p");
+  showDateLabel.classList.add("show-details--date-label");
+  showDate.appendChild(showDateLabel);
+  showDateLabel.innerText = "Date";
+
+  const showDateInput = document.createElement("h3");
+  showDateInput.classList.add("show-details--date-input");
+  showDate.appendChild(showDateInput);
+  showDateInput.innerText = "Shows[i].date";
+  // venue
+  const showVenue = document.createElement("section");
+  showVenue.classList.add("show-details--venue");
+  showEl.appendChild(showVenue);
+
+  const showVenueLabel = document.createElement("p");
+  showVenueLabel.classList.add("show-details--venue-label");
+  showVenue.appendChild(showVenueLabel);
+  showVenueLabel.innerText = "Venue";
+
+  const showVenueInput = document.createElement("p");
+  showDateInput.classList.add("show-details--venue-input");
+  showVenue.appendChild(showVenueInput);
+  showVenueInput.innerText = "Shows[show].venue";
+
+  // location
+  const showLocation = document.createElement("section");
+  showLocation.classList.add("show-details--location");
+  showEl.appendChild(showLocation);
+
+  const showLocationLabel = document.createElement("p");
+  showLocationLabel.classList.add("show-details--location-label");
+  showLocation.appendChild(showLocationLabel);
+  showLocationLabel.innerText = "Shows[i].location";
+
+  const showLocationInput = document.createElement("p");
+  showLocationInput.classList.add("show-details--location-input");
+  showLocation.appendChild(showLocationInput);
+  showLocationInput.innerText = "Shows[i].location";
+
+  const showButton = document.createElement("Button");
+  showButton.classList.add("show-details--button");
+  showEl.appendChild(showButton);
+  showButton.innerText = "Buy Tickets";
+
+  const Divider = document.createElement("hr");
+  Divider.classList.add("show__divider");
+  showCont.appendChild(Divider);
+}
+for (let i = 0; i < Shows.length; i++) {
+  CreateShow(i);
+}
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,23 +1,75 @@
 const Comments = [
   {
-    image: URL(),
     name: "Connor Walton",
-    date: "02/17/2021",
-    comment:
+    date: 1613500200000,
+    siteComment:
       "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
   },
   {
-    image: URL(),
     name: "Emilie Beach",
-    date: "01/09/2021",
-    comment:
+    date: 1610130600000,
+    siteComment:
       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
   },
   {
-    image: URL(),
     name: "Miles Acosta",
-    date: "12/02/2020",
-    comment:
+    date: 1606847400000,
+    siteComment:
       "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
   },
 ];
+
+CreateComment();
+
+function CreateComment() {
+  const commentCont = document.querySelector(".comments");
+  commentCont.innerHTML = "";
+  Comments.forEach((comment) => {
+    const commentEl = document.createElement("article");
+    commentEl.classList.add("comment-details");
+    commentCont.appendChild(commentEl);
+
+    const commentImage = document.createElement("img");
+    commentImage.classList.add("comment-details--image");
+    commentImage.setAttribute("src", "");
+    commentEl.appendChild(commentImage);
+
+    const commentContainer = document.createElement("section");
+    commentContainer.classList.add("comment-details--sec");
+    commentEl.appendChild(commentContainer);
+
+    const commentName = document.createElement("p");
+    commentName.classList.add("comment-details--sec-name");
+    commentContainer.appendChild(commentName);
+    commentName.innerText = comment.name;
+
+    const commentDate = document.createElement("p");
+    commentDate.classList.add("comment-details--sec-date");
+    commentContainer.appendChild(commentDate);
+    commentDate.innerText = new Date(comment.date).toLocaleDateString("en-US");
+
+    const commentText = document.createElement("p");
+    commentText.classList.add("comment-details--sec-text");
+    commentContainer.appendChild(commentText);
+    commentText.innerText = "comment.siteComment";
+
+    const Divider = document.createElement("hr");
+    Divider.classList.add("comments__divider");
+    commentCont.appendChild(Divider);
+  });
+}
+CreateComment();
+const Form = document.querySelector(".comment-section__form");
+
+Form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const newName = event.target.name;
+  const newComment = event.target.comment;
+  Comments.unshift({
+    name: newName.value,
+    date: Date.now(),
+    comment: newComment.value,
+  });
+  event.target.reset();
+  CreateComment();
+});
